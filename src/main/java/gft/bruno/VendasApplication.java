02eb1f90e585486b7312799f3c250ca4097489c8
Bruno Yuji Yoshioka <bruno.yuji1995@gmail.com>
2020-04-22 23:55:59 -0300

    Configuração externalizada: application.properties

--- src/main/java/gft/bruno/VendasApplication.java
+++ src/main/java/gft/bruno/VendasApplication.java
@@ -2,6 +2,7 @@ package gft.bruno;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,8 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 
-    @Autowired
-    @Qualifier("applicationName")
+    @Value("${application.name}")
     private String applicationName;
 
     @GetMapping("/hello")
