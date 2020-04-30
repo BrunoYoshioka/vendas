c31760ef0ff1738df053d7d069d604bd91cc30d4
Bruno Yuji Yoshioka <bruno.yuji1995@gmail.com>
2020-04-30 15:22:10 -0300

    Criando as classes de modelo

---
+++ src/main/java/gft/bruno/domain/entity/Pedido.java
@@ -0,0 +1,43 @@
+package gft.bruno.domain.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Pedido {
+    private Integer id;
+    private Cliente cliente;
+    private LocalDate dataPedido;
+    private BigDecimal total;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDate getData() {
+        return dataPedido;
+    }
+
+    public void setData(LocalDate data) {
+        this.dataPedido = data;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+}
